cb34dfc73cee911926785be42f887c08ad3954c5
Rafael <rafasousa@gmail.com>
2021-05-24 09:03:14 -0300

    utiliza ResponseEntityExceptionHandler para capturar os erros

--- src/main/java/br/com/zup/orangetalents/mercadolivre/comum/exception/ErroRequisicaoHandler.java
+++ src/main/java/br/com/zup/orangetalents/mercadolivre/comum/exception/ErroRequisicaoHandler.java
@@ -5,25 +5,27 @@ import java.util.List;
 
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
 
 @RestControllerAdvice
-public class ErroRequisicaoHandler {
+public class ErroRequisicaoHandler extends ResponseEntityExceptionHandler {
 	
 	private MessageSource messageSource;
-
+	
 	public ErroRequisicaoHandler(MessageSource messageSource) {
-		super();
 		this.messageSource = messageSource;
 	}
-
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
-	public List<Erro> tratar(MethodArgumentNotValidException ex) {
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
 		List<Erro> erros = new ArrayList<>();
 		
 		ex.getGlobalErrors().forEach(globalError -> {
@@ -37,9 +39,39 @@ public class ErroRequisicaoHandler {
 			erros.add(new Erro(campo, mensagem));
 		});
 		
-		return erros;
+		return new ResponseEntity<Object>(erros, status);
 	}
 }
+
+//@RestControllerAdvice
+//public class ErroRequisicaoHandler {
+//	
+//	private MessageSource messageSource;
+//
+//	public ErroRequisicaoHandler(MessageSource messageSource) {
+//		super();
+//		this.messageSource = messageSource;
+//	}
+//
+//	@ExceptionHandler(MethodArgumentNotValidException.class)
+//	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+//	public List<Erro> tratar(MethodArgumentNotValidException ex) {
+//		List<Erro> erros = new ArrayList<>();
+//		
+//		ex.getGlobalErrors().forEach(globalError -> {
+//			erros.add(new Erro(globalError.getDefaultMessage()));
+//		});
+//		
+//		ex.getFieldErrors().forEach(fieldError -> {
+//			String campo = fieldError.getField();
+//			String mensagem = messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
+//			
+//			erros.add(new Erro(campo, mensagem));
+//		});
+//		
+//		return erros;
+//	}
+//}
 
 class Erro {
 	
